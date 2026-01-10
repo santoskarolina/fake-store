49f88ad4d9dd4a4bc7703dfff9ffec06d01e1f59
santoskarolina <anna15soliveira@gmail.com>
2026-01-10 17:39:28 -0300

    fix: update cache on fetch

--- src/app/_services/api.ts
+++ src/app/_services/api.ts
@@ -4,13 +4,17 @@ import { ProductDetails } from "../_types/product-details";
 const BASE_URL: string = 'https://fakestoreapi.com/products';
 
 export const getProducts = async (): Promise<Product[]> => {
-  const response = await fetch(BASE_URL);
+  const response = await fetch(BASE_URL, {
+    cache: 'no-store',
+  });
   if (!response.ok) return []
   return response.json();
 };
 
 export const getProductDetails= async (id: number): Promise<ProductDetails> => {
-  const response = await fetch(`${BASE_URL}/${id}`);
-  if (!response.ok) return { category: '', description: '', id: 0, image: '', price: 0, rating: { count:1, rate: 1} , title:''};
+  const response = await fetch(`${BASE_URL}/${id}`, {
+    cache: 'no-store',
+  });
+  if (!response.ok) return { category: '', description: '', id: 0, image: '', price: 0, rating: { count:1, rate: 1},  title: ''};
   return response.json();
 };
