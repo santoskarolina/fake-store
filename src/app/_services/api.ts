07978f5051a9672346f993676deb4581396f754a
santoskarolina <anna15soliveira@gmail.com>
2026-01-10 17:46:50 -0300

    fix: update options on api

--- src/app/_services/api.ts
+++ src/app/_services/api.ts
@@ -3,18 +3,22 @@ import { ProductDetails } from "../_types/product-details";
 
 const BASE_URL: string = 'https://fakestoreapi.com/products';
 
+const options: RequestInit = {
+  cache: 'no-store',
+  headers: {
+    'User-Agent': 'Mozilla/5.0',
+    'Content-Type': 'application/json'
+  }
+}
+
 export const getProducts = async (): Promise<Product[]> => {
-  const response = await fetch(BASE_URL, {
-    cache: 'no-store',
-  });
+  const response = await fetch(BASE_URL, options);
   if (!response.ok) return []
   return response.json();
 };
 
-export const getProductDetails= async (id: number): Promise<ProductDetails> => {
-  const response = await fetch(`${BASE_URL}/${id}`, {
-    cache: 'no-store',
-  });
-  if (!response.ok) return { category: '', description: '', id: 0, image: '', price: 0, rating: { count:1, rate: 1},  title: ''};
+export const getProductDetails = async (id: number): Promise<ProductDetails> => {
+  const response = await fetch(`${BASE_URL}/${id}`, options);
+  if (!response.ok) return { category: '', description: '', id: 0, image: '', price: 0, rating: { count: 1, rate: 1 }, title: '' };
   return response.json();
 };
