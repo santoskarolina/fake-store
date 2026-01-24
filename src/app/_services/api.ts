28e00f23c3dda7048392532670ea357293cd0a8f
santoskarolina <anna15soliveira@gmail.com>
2026-01-24 18:14:11 -0300

    fix: user agent

--- src/app/_services/api.ts
+++ src/app/_services/api.ts
@@ -6,7 +6,7 @@ const BASE_URL: string = 'https://fakestoreapi.com/products';
 const options: RequestInit = {
   cache: 'no-store',
   headers: {
-    'User-Agent': 'Mozilla/5.0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
     'Content-Type': 'application/json'
   }
 }
